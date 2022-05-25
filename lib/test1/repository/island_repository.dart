ca41719c7b909248164d2ef9d84055d99fbaa9be
Jaime Norato <jn901113jn@gmail.com>
2022-05-25 06:26:46 -0500

    vista de islas finalizada

--- lib/test1/repository/island_repository.dart
+++ lib/test1/repository/island_repository.dart
@@ -6,37 +6,43 @@ class IslandRepository {
 
   final int size;
 
-  late List<List<int>> mapIslands;
+  late List<List<int>> _mapIslands;
 
   late List<Map> vertices = [];
   late List<List<Map>> islands = [];
 
   /// genera un mapa de islas de numeros aleatorios con el tamaño indicado
   List<List<int>> generateMap(){
-    print("generando mapa de islas");
-    print(size);
-    // mapIslands = List<List<int>>.generate(size, (i) => List<int>.generate(size, (j) => Random().nextInt(2)));
-    mapIslands = [
-      [1,1,0,1,1],
-      [1,1,0,0,0],
-      [0,1,0,0,0],
-      [0,1,0,0,1],
-      [1,1,0,1,1],
-    ];
-    print(mapIslands);
-    return mapIslands;
+    _mapIslands = List<List<int>>.generate(size, (i) => List<int>.generate(size, (j) => Random().nextInt(2)));
+    // mapIslands = [
+    //   [1,1,0,1,1],
+    //   [1,1,0,0,0],
+    //   [0,1,0,0,0],
+    //   [0,1,0,0,1],
+    //   [1,1,0,1,1],
+    // ];
+    vertices = [];
+    islands = [];
+    return _mapIslands;
+  }
+  /// actualiza el mapa de islas con el nuevo valor
+  set mapIslands(List<List<int>> newMap){
+    _mapIslands = newMap;
+    vertices = [];
+    islands = [];
   }
   /// realiza el conteo de islas
-  int countIslands(){
+  Map<String, int> countIslands(){
     searchVertices();
     mapsIslands();
-    return vertices.length;
+    validateVertices();
+    return {"vertices": vertices.length, "islands": islands.length};
   }
   /// busca las vertices de las islas
   searchVertices(){
     for(int i = 0; i < size; i++){
       for(int j = 0; j < size; j++){
-        if(mapIslands[i][j] == 1 && isVertex(i, j)){
+        if(_mapIslands[i][j] == 1 && isVertex(i, j)){
           vertices.add({'x': i, 'y': j});
         }
       }
@@ -45,7 +51,7 @@ class IslandRepository {
   /// valida si en la posicion indicada es un vertice
   isVertex(int i, int j){
     // if((i == 0 || mapIslands[i - 1][j] == 0) && (j == 0 || mapIslands[i][j - 1] == 0) && (i == size - 1 || mapIslands[i + 1][j] == 0) && (j == size - 1 || mapIslands[i][j + 1] == 0)){
-    return (i == 0 || mapIslands[i - 1][j] == 0) && (j == 0 || mapIslands[i][j - 1] == 0);
+    return (i == 0 || _mapIslands[i - 1][j] == 0) && (j == 0 || _mapIslands[i][j - 1] == 0);
   }
   /// valida el listado de vertices, para saber si es una isla
   validateVertices(){
@@ -75,8 +81,10 @@ class IslandRepository {
   mappingIsland(List<Map> island, int x, int y){
     for(int i = x; i < size; i++){
       for(int j = y; j < size; j++){
-        if(mapIslands[i][j] == 1 && !isVertex(i,j)){
+        if(_mapIslands[i][j] == 1 && !isVertex(i,j)){
           if(i != 0 && inIsland(island, i-1, j) || j != 0 && inIsland(island, i, j-1)){
+            island.add({'x': i, 'y': j});
+          }else if(inIsland(island, i+1, j) || inIsland(island, i, j+1)){
             island.add({'x': i, 'y': j});
           }
         }
--- lib/test1/screens/islands.dart
+++ lib/test1/screens/islands.dart
@@ -14,7 +14,7 @@ class _IslandsState extends State<Islands> {
 
   late IslandRepository _islandRepository;
   late List<List<int>> mapIslands;
-  late int totalIslands = 0;
+  late Map<String, int> totals = {};
   late bool existMap = false;
 
   @override
@@ -34,6 +34,8 @@ class _IslandsState extends State<Islands> {
               decoration: const InputDecoration(
                 hintText: 'Ingrese el número de islas',
               ),
+              style: const TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
             ),
             const SizedBox(height: 10),
             ElevatedButton(
@@ -42,14 +44,19 @@ class _IslandsState extends State<Islands> {
                 _islandRepository = IslandRepository(int.parse(_textNumberController.text));
                 setState(() {
                   mapIslands = _islandRepository.generateMap();
-                  totalIslands = _islandRepository.countIslands();
+                  totals = _islandRepository.countIslands();
                   existMap = true;
                 });
               },
             ),
             const SizedBox(height: 15),
             Text(
-              'Total de islas: $totalIslands',
+              'Total de vértices de islas: ${totals['vertices']??0}',
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 15),
+            Text(
+              'Total de islas mapeadas: ${totals['islands']??0}',
               style: const TextStyle(fontSize: 20),
             ),
             const SizedBox(height: 15),
@@ -63,24 +70,39 @@ class _IslandsState extends State<Islands> {
   }
 
   Widget buildGraph(Size size) {
-    return Center(
+    return Padding(
+      padding: EdgeInsets.only(left: size.width * 0.2, right: size.width * 0.1),
       child: Column(
-        children: mapIslands.map((List<int> list) {
-          return Row(
-            children: list.map((int value) {
-              return Container(
-                width: size.width / mapIslands.length * 0.2,
-                height: size.height / mapIslands.length * 0.2,
-                color: value == 1 ? Colors.red : Colors.blue,
-                child: TextField(
-                  enabled: false,
-                  controller: TextEditingController(text: value.toString()),
-                ),
-              );
-            }).toList(),
-          );
-        }).toList(),
+        children: buildMap(size),
       ),
     );
+  }
+
+  List<Row> buildMap(Size size) {
+    final List<Row> rows = [];
+    for(int i = 0; i < mapIslands.length; i++) {
+      final List<Widget> widgets = [];
+      for (int j = 0; j < mapIslands[i].length; j++) {
+        widgets.add(
+          Container(
+            width: size.width / mapIslands.length * 0.5,
+            height: size.height / mapIslands.length * 0.3,
+            color: mapIslands[i][j] == 1 ? Colors.red : Colors.blue,
+            child: TextField(
+              controller: TextEditingController(text: mapIslands[i][j].toString()),
+              onChanged: (value) {
+                setState(() {
+                  mapIslands[i][j] = int.parse(value.toString());
+                  _islandRepository.mapIslands = mapIslands;
+                  totals = _islandRepository.countIslands();
+                });
+              },
+            ),
+          ),
+        );
+      }
+      rows.add(Row(children: widgets));
+    }
+    return rows;
   }
 }
